1e4db714d3a3f21f0b67e79fd9b8f2eeeff37b51
Exequiel Alberto Castro Rivero <exequiel_98@hotmail.es>
2024-06-05 18:15:54 +0000

    [feature/database-setup] Adds dbConfig file

---
+++ web-crawler-app/src/config/dbConfig.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+};
