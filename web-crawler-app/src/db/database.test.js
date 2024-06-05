727b371d00475edd069da3fd45c692a17bf3c008
Exequiel Alberto Castro Rivero <exequiel_98@hotmail.es>
2024-06-05 19:00:25 +0000

    [feature/database-setup] Adds TDD tests to test tables created

--- web-crawler-app/src/db/database.test.js
+++ web-crawler-app/src/db/database.test.js
@@ -1,16 +1,53 @@
-const { sequelize, authenticateDB } = require('./database');
+const { sequelize, authenticateDB, createTablesIfNotExist, Entries, Filters, UsageData } = require('./database');
 
-describe('Database Connection', () => {
+describe('Database Tests', () => {
     afterAll(async () => {
         await sequelize.close();
-      });
-      
-    it('should authenticate successfully', async () => {
-        await expect(authenticateDB()).resolves.toBeUndefined();
     });
 
-    it('should be able to perform operations', async () => {
-        const result = await sequelize.query('SELECT 1+1 AS result');
-        expect(result[0][0].result).toBe(2);
+    describe('Connection', () => {
+        it('should authenticate successfully', async () => {
+            await expect(authenticateDB()).resolves.toBeUndefined();
+          });
+        
+          it('should be able to perform operations', async () => {
+            const result = await sequelize.query('SELECT 1+1 AS result');
+            expect(result[0][0].result).toBe(2);
+          });
+        
+          it('should create tables if not exist', async () => {
+            await expect(createTablesIfNotExist()).resolves.toBeUndefined();
+          });
+    });
+
+    describe('Models', () => {
+        it('should have an Entries model', () => {
+            expect(Entries).toBeDefined();
+        });
+
+        it('should have a Filters model', () => {
+            expect(Filters).toBeDefined();
+        });
+
+        it('should have a UsageData model', () => {
+            expect(UsageData).toBeDefined();
+        });
+    });
+
+    describe('Tables', () => {
+        it('should have an Entry table', async () => {
+            const result = await sequelize.query('SELECT * FROM "Entries"');
+            expect(result).toBeDefined();
+        });
+
+        it('should have a Filter table', async () => {
+            const result = await sequelize.query('SELECT * FROM "Filters"');
+            expect(result).toBeDefined();
+        });
+
+        it('should have a UsageData table', async () => {
+            const result = await sequelize.query('SELECT * FROM "UsageData"');
+            expect(result).toBeDefined();
+        });
     });
 });
