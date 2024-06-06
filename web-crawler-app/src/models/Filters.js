4ce94fbb777032c1f8ee99ba609590b723c3017f
Exequiel Alberto Castro Rivero <exequiel_98@hotmail.es>
2024-06-06 16:02:17 +0000

    [feature/filters] Redefines filters db model

--- web-crawler-app/src/models/Filters.js
+++ web-crawler-app/src/models/Filters.js
@@ -2,7 +2,10 @@
 
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('Filters', {
-        name: DataTypes.STRING,
+        name: {
+            type: DataTypes.STRING,
+            unique: true
+        },
         description: DataTypes.STRING
     });
 };
