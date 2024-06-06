ebe9770923de181a5a99e841b556eb5bd08cae1f
Exequiel Alberto Castro Rivero <exequiel_98@hotmail.es>
2024-06-06 20:57:12 +0000

    [feature/server-port-forward] Moves ports to .env config

--- web-crawler-app/src/config/appConfig.js
+++ web-crawler-app/src/config/appConfig.js
@@ -1,7 +1,6 @@
 const appConfig = {
     url: 'https://news.ycombinator.com/',
     entries: 30,
-    serverPort: 4200,
     commentsFilter: {
         name: 'comments',
         description: 'Filter entries by comments',
